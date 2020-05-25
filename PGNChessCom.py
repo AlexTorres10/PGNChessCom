c6b803bb1005d37dfb0fc5830f4480e30eb31ef3
Alex Torres <alextcarvalho98@gmail.com>
2020-05-25 19:56:29 -0400

    Pequenos ajustes após testes de estresse.

--- PGNChessCom.py
+++ PGNChessCom.py
@@ -3,17 +3,22 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 import time, math, pyautogui as gui
 
-# Esteja já logado no site
-gameurl = "https://www.chess.com/games/archive?gameOwner=my_game&gameType=live&gameTypeslive%5B%5D=blitz&gameTypeslive%5B%5D=rapid&timeSort=desc&page="
 
 login = ""
 password = ""
 
 loginUrl = "https://www.chess.com/login_and_go?returnUrl=https%3A%2F%2Fwww.chess.com%2F"
-
+# Esteja já logado no site
+#gameurl = "https://www.chess.com/games/archive?gameOwner=my_game&gameType=live&gameTypeslive%5B%5D=blitz&gameTypeslive%5B%5D=rapid&timeSort=desc"
+gameurl = "https://www.chess.com/games/archive/tpros?show=live_blitz&gameType=live_blitz&rated=rated"
 # Diga quantos jogos quer baixar
-games = 657
+games = 0
+# Que eu converto para quantas páginas vai precisar
 pages = math.ceil(games/50)
+# O site só vai até a página 101. Esse 'if' evita que ele fique baixando o mesmo PGN.
+if (pages > 101):
+    pages = 101
+
 
 browser = None
 # Ele primeiro vai tentar o Chrome
@@ -21,47 +26,49 @@ isChrome = True
 # Deixe como False se estiver com Windows
 isLinux = False
 
-try:
-    caps = DesiredCapabilities().CHROME.copy()
-    caps["pageLoadStrategy"] = "eager" # interactive
-    chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-extensions')
-    # Coloque o caminho de onde você botou o ChromeDriver aqui.
-    if (isLinux):
-        browser = webdriver.Chrome(options=chrome_options,
-        executable_path="/home/alextorres10/chromedriver")
-    else:
-        browser = webdriver.Chrome(options=chrome_options,
-        executable_path="C:\\chromedriver.exe")
-    
-except:
-    isChrome = False
-    caps = DesiredCapabilities().FIREFOX.copy()
-    caps["pageLoadStrategy"] = "eager"
-    bp = webdriver.FirefoxProfile()
-    bp.set_preference("dom.webnotifications.enabled", False)
-    if (isLinux):
-        browser = webdriver.Firefox(capabilities=caps, firefox_profile=bp)
-    else:
-        browser = webdriver.Firefox(capabilities=caps, firefox_profile=bp, 
-            executable_path=r'C:\\geckodriver.exe')
-	# Da mesma forma, coloque o caminho do Geckodriver.
+if (not(login == '' or password == '' or games == 0)):
+    try:
+        caps = DesiredCapabilities().CHROME.copy()
+        caps["pageLoadStrategy"] = "eager" # interactive
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-extensions')
+        # Coloque o caminho de onde você botou o ChromeDriver aqui.
+        if (isLinux):
+            browser = webdriver.Chrome(options=chrome_options,
+            executable_path="/home/alextorres10/chromedriver")
+        else:
+            browser = webdriver.Chrome(options=chrome_options,
+            executable_path="C:\\chromedriver.exe")
+        
+    except:
+        isChrome = False
+        caps = DesiredCapabilities().FIREFOX.copy()
+        caps["pageLoadStrategy"] = "eager"
+        bp = webdriver.FirefoxProfile()
+        bp.set_preference("dom.webnotifications.enabled", False)
+        if (isLinux):
+            browser = webdriver.Firefox(capabilities=caps, firefox_profile=bp)
+        else:
+            browser = webdriver.Firefox(capabilities=caps, firefox_profile=bp, 
+                executable_path=r'C:\\geckodriver.exe')
+        # Da mesma forma, coloque o caminho do Geckodriver.
 
-browser.get(loginUrl)
+    browser.get(loginUrl)
+    browser.find_element_by_id('username').send_keys(login)
+    browser.find_element_by_id('password').send_keys(password)
+    browser.find_element_by_id('login').click()
 
-browser.find_element_by_id('username').send_keys(login)
-browser.find_element_by_id('password').send_keys(password)
-browser.find_element_by_id('login').click()
-
-for i in range(1,pages+1):
-    url = gameurl + str(i)
-    browser.get(url)
-    browser.find_element_by_class_name('archive-games-check-all').click()
-    time.sleep(1)
-    browser.find_element_by_class_name('archive-games-download-button').click()
-    time.sleep(5)
-    if (not isChrome):
-        if (i == 1):
-            gui.press('down')
-        gui.press('enter')
+    for i in range(1,pages+1):
+        url = gameurl + "&page=" + str(i)
+        browser.get(url)
+        browser.find_element_by_class_name('archive-games-check-all').click()
+        time.sleep(1)
+        browser.find_element_by_class_name('archive-games-download-button').click()
+        time.sleep(5)
+        if (not isChrome):
+            if (i == 1):
+                gui.press('down')
+            gui.press('enter')
+else:
+    print("Estão faltando dados ou você não especificou quantos jogos vai baixar.")
